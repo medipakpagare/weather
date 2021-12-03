386fa656c33f0234dc23a05cf15172917c4b6069
7ORP3DO <davisudha806@gmail.com>
2021-12-03 10:13:54 +0530

    Get city data using ipinfo API

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
 let inputValCity = document.getElementById("city");
 const showData = document.getElementById("showWeather");
-const temp=document.getElementById('temp')
-const weatherType=document.getElementById('weatherType')
-const weatherDesc=document.getElementById('weatherDesc')
-const country=document.getElementById('country')
+const temp = document.getElementById('temp')
+const weatherType = document.getElementById('weatherType')
+const weatherDesc = document.getElementById('weatherDesc')
+const country = document.getElementById('country')
+
+window.onload = () => {
+  getCity();
+}
+
+function getCity() {
+  $.ajax({
+    url: "https://ipinfo.io/json",
+    beforeSend: function () {
+      $("#city").attr("placeholder", "Detecting your city...");
+    },
+    success: function (data) {
+      $("#city").val(data.city);
+      getWeather();
+    },
+    complete: function () {
+      $("#city").attr("placeholder", "Enter your city name");
+    },
+    dataType: "JSON"
+  });
+}
+
 
 let weatherBtn = document
   .querySelector("button")
   .addEventListener("click", function (e) {
     e.preventDefault();
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValCity.value}&appid=131c028170b0cf867703265c71524ce8`;
-    fetch(url)
-      .then((res) => res.json())
-   
-      .then((data) => {
-try{
-       //for temp
-    temp.innerHTML=data.main.temp
-    
-    //for weather type
-    weatherType.innerHTML=data.weather.map((weatherTypes=>weatherTypes.main))
-        
-    //for weather description
-    weatherDesc.innerHTML=data.weather.map(weatherTypes=>weatherTypes.description)
-
-      //for country
-      country.innerHTML=data.sys.country
-}
-catch(err){
-  alert("Please enter city name correctly or something went wrong from our side. Soryy for inconvenience.")
-}
-      });
-  
-    
-     
+    getWeather();
   });
-  
+
+
+function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValCity.value}&appid=131c028170b0cf867703265c71524ce8`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      try {
+        //for temp
+        temp.innerHTML = data.main.temp
+
+        //for weather type
+        weatherType.innerHTML = data.weather.map((weatherTypes => weatherTypes.main))
+
+        //for weather description
+        weatherDesc.innerHTML = data.weather.map(weatherTypes => weatherTypes.description)
+
+        //for country
+        country.innerHTML = data.sys.country
+      }
+      catch (err) {
+        alert("Please enter city name correctly or something went wrong from our side. Soryy for inconvenience.")
+      }
+    });
+}
